7b73d9656012b909b18924ca60c766b9272829f3
fd7mhvrh69-boop <fd7mhvrh69@privaterelay.appleid.com>
2026-09-20 15:08:16 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'coin-flip-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install the service worker and cache files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
